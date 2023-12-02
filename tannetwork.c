d88437f9527d2d65e0cf767027593d9694fcb629
yoonseongan <125129388+yoonseongan@users.noreply.github.com>
2023-12-02 19:18:19 +0900

    Update tannetwork.c

--- tannetwork.c
+++ tannetwork.c
@@ -18,7 +18,7 @@ double toRad(double deg) {
     return (deg / 180) * M_PI;
 }       //각도를 라디안으로
 
-void initStop(Stop* stop, char stopinfo[]) {
+void initStop(Stop* stop, char stopinfo[]) { //Stop 구조체를 초기화하는 함수 문자열(stopinfo[])을 파싱하여 구조체의 각 필드에 할당
     int idx = 0;
     while (stopinfo[++idx] != ',') {}
     stop->id = (char*)malloc(++idx);
@@ -95,19 +95,19 @@ void initStop(Stop* stop, char stopinfo[]) {
 
 int cmp(const void* _lhs, const void* _rhs) {
     Stop* lhs = (Stop*)_lhs, * rhs = (Stop*)_rhs;
-    return strcmp(lhs->id, rhs->id) > 0;
+    return strcmp(lhs->id, rhs->id) > 0;//qsort 함수에서 사용하는 비교 함수 Stop 구조체의 id를 기준으로 오름차순 정렬
 }
 
 int bicmp(const void* _lhs, const void* _rhs) {
     Stop* lhs = (Stop*)_lhs, * rhs = (Stop*)_rhs;
     return strcmp(lhs->id, rhs->id);
-}
+}//bsearch 함수에서 사용하는 비교 Stop 구조체의 id를 기준으로 이진 검색을 수행
 
 double getDist(Stop* lhs, Stop* rhs) {
     double x = (rhs->x - lhs->x) * cos((rhs->y + lhs->y) / 2);
     double y = rhs->y - lhs->y;
     return sqrt(x * x + y * y) * 6371;
-}
+}//두 정류장 간의 거리를 계산 위도와 경도를 이용하여 거리를 계산
 
 typedef struct Edge {
     double cost;
@@ -129,14 +129,14 @@ struct element {
 void initList(list* ls) {
     ls->begin = (element*)malloc(sizeof(element));
     ls->end = ls->begin;
-}
+}//연결 리스트를 초기화하는 역할 새로운 element를 동적 할당하여 리스트의 시작과 끝을 가리키게 하는 역할
 
 void emplace_back(list* ls, int t, double cost) {
     ls->end->next = (element*)malloc(sizeof(element));
     ls->end->item.dst = t;
     ls->end->item.cost = cost;
     ls->end = ls->end->next;
-}
+}// 연결리스트에 새로운 요소 추가, 동적할당으로, 목적지와 비용을 가지고 잇음
 
 
 typedef struct pq {
@@ -208,6 +208,13 @@ bool solve(int S, int E, int N) {
         chk[i] = false;
         from[i] = i;
     }
+    /*이 함수는 시작 정류장(S)에서 종점 정류장(E)까지의 최단 경로를 찾는 역할
+     다익스트라 알고리즘
+     모든 정류장의 거리를 무한대로 초기화한 후, 시작 정류장의 거리를 0으로 설정 그리고 우선순위 큐에 시작 정류장 추가
+     우선순위 큐가 빌 때까지 다음의 과정을 반복
+     우선순위 큐에서 가장 거리가 짧은 정류장을 추출
+     해당 정류장에서 갈 수 있는 모든 정류장에 대해, 현재 정류장을 거쳐 가는 거리가
+     기존의 거리보다 짧으면 거리를 업데이트 해당 정류장을 우선순위 큐에 추가*/
     dists[S] = 0;
     Edge edge;
     edge.cost = 0;
